d2f4cb4962a5d04a25e62255eac47bb4776405f8
Rafal Dabrowski <rafal.dabrowski@pagepro.pl>
2022-06-21 20:51:31 +0200

    sdasdas :Wq

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { NextQueryParamProvider } from "next-query-params";
+import type { AppProps } from "next/app";
+import React from "react";
+import "./global.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <NextQueryParamProvider>
+      <Component {...pageProps} />
+    </NextQueryParamProvider>
+  );
 }
-export default MyApp
+export default MyApp;
---
+++ pages/components/NavBar/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const NavBar: React.FC = () => {
+  return (
+    <nav
+      style={{
+        position: "sticky",
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        top: 0,
+        left: 0,
+        right: 0,
+        height: 80,
+        padding: "0 40px",
+        background: "#F4F5FF",
+      }}
+    >
+      {/* empty */}
+    </nav>
+  );
+};
+
+export default NavBar;
